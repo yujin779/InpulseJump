cee2c0c6952b96cc6fa623020f3bc8232fb3a279
yujin779 <51077021+yujin779@users.noreply.github.com>
2020-12-31 07:47:10 +0900

    α

--- src/App.js
+++ src/App.js
@@ -1,47 +1,60 @@
 // App.js
 import React, { useState } from "react";
-import * as THREE from "three";
 import { View } from "react-native";
 import { Canvas, useFrame } from "react-three-fiber";
 import CameraController from "./components/CameraController";
 import { Physics, useBox } from "use-cannon";
-import { useStore } from "./Global";
 import styles from "./styles";
-
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import { useLoader } from "react-three-fiber";
 import dino from "./dino.glb";
+import create from "zustand";
+
+const useStore = create((set) => ({
+  //画面がクリックされたらtrue
+  tap: false,
+  tapTrue: () => set((state) => ({ tap: true })),
+  tapFalse: () => set((state) => ({ tap: false }))
+}));
 
 /*
- * タップするとジャンプするプレイヤー
+ * ジャンプするダイナソー
  */
-const Player = () => {
-  const args = [4, 1, 4];
+const Dino = () => {
+  // 物理演算させるボックスのサイズ
+  const args = [2, 2, 1];
   const gltf = useLoader(GLTFLoader, dino);
-  // console.log(gltf);
+  // tapのstateをfalseに変更するfunction
   const tapFalse = useStore((state) => state.tapFalse);
+  // tapのstateの値
   const tap = useStore((state) => state.tap);
+  // 床に着地していればtrue
   const [landing, setLanding] = useState(false);
+  // useBoxを使用してボックス型の物理演算を設定
   const [ref, api] = useBox(() => ({
     mass: 1,
     args: args,
-    position: [-3, 3, 0],
+    // ダイナソーの位置
+    position: [-0.5, 3, 0],
+    // 床に着地していればtrue
     onCollide: (obj) => {
       if (obj.body.name === "floor") setLanding(true);
-      if (obj.body.name === "enemy") {
-        console.log("gameover");
-      }
     }
   }));
   useFrame((state) => {
+    // 床に着地してクリックするとジャンプ
     if (tap && landing) {
+      // ジャンプ
       api.applyImpulse([0, 20, 0], [0, 0, 0]);
+      // tapのstateをfalseに変更
       tapFalse();
+      // landingのstateをfalseに変更
       setLanding(false);
     }
   });
   return (
     <group ref={ref}>
+      {/* 物理演算させるボックスを表示 */}
       <mesh name="player">
         <boxBufferGeometry attach="geometry" args={args} />
         <meshStandardMaterial
@@ -51,11 +64,15 @@ const Player = () => {
           opacity={0.3}
         />
       </mesh>
+      {/* ダイナソーのオブジェクト */}
       <primitive object={gltf.scene} position={[-0.4, -1, 0]} />
     </group>
   );
 };
 
+/**
+ * 床
+ */
 const Floor = ({ position, args, color }) => {
   const [ref] = useBox(() => ({
     type: "Static",
@@ -67,7 +84,7 @@ const Floor = ({ position, args, color }) => {
   return (
     <mesh ref={ref} name="floor">
       <boxBufferGeometry attach="geometry" args={args} />
-      <meshStandardMaterial attach="material" color={"hotpink"} />
+      <meshStandardMaterial attach="material" color={"green"} />
     </mesh>
   );
 };
@@ -76,42 +93,30 @@ const Floor = ({ position, args, color }) => {
  * 1. 表示される入り口
  */
 const App = () => {
-  // const [, setJump] = useGlobalState("tap");
-  // const steteTap = useStore((state) => state.tap);
   const tapTrue = useStore((state) => state.tapTrue);
   return (
     <View style={styles.app}>
       <Canvas
         camera={{
           position: [-0.2, 3, 10],
-          // position: [0, 30, 0],
-          // lookAt: [0, 0, 0],
-          // rotation: [0.1, -0.2, 0],
-          // quaternion: [0.0, -0.1, 0.0],
           near: 0.1,
           far: 50
         }}
+        // tapのstateをtrueに変更
         onClick={(e) => {
           tapTrue();
         }}
-        // onCreated={({ camera }) => {
-        //   console.log(camera);
-        //   camera.lookAt(900, 90, 0);
-        //   // camera.updateProjectionMatrix();
-        // }}
       >
         <ambientLight intensity={0.5} />
-        <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
+        <spotLight position={[8, 20, 9]} angle={0.15} penumbra={1} />
         <pointLight position={[-10, -10, -10]} />
         <CameraController />
         <Physics
           gravity={[0, -30, 0]}
           defaultContactMaterial={{ restitution: 0 }}
         >
-          <Player />
-          {/* <Enemy />
-          <Enemy /> */}
-          <Floor position={[0, -1, 0]} args={[1500, 0.5, 3]} />
+          <Dino />
+          <Floor position={[0, -1, 0]} args={[500, 0.5, 500]} />
         </Physics>
       </Canvas>
     </View>
